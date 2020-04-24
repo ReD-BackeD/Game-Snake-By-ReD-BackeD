ea101cc786a7716da57984977156330eda55c0b1
ReD-BackeD <62339756+ReD-BackeD@users.noreply.github.com>
2020-04-24 23:26:33 +0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -16,8 +16,8 @@ high_score = 0
 
 # Set up the screen
 wn = turtle.Screen()
-wn.title("Snake Game by @ReDBackeD")
-wn.bgcolor("green")
+wn.title("Snake Game by @ReD-BackeD")
+wn.bgcolor("white")
 wn.setup(width=600, height=600)
 wn.tracer(0) # Turns off the screen updates
 
@@ -44,7 +44,7 @@ segments = []
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
-pen.color("white")
+pen.color("yellow")
 pen.penup()
 pen.hideturtle()
 pen.goto(0,260)
